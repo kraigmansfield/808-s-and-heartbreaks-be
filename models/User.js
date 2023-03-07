c513d4292711aac965f6a22988d51e7f22d3a457
kraigmansfield <Kraig.mansfield@gmail.com>
2023-03-07 10:48:37 -0800

    added User model

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init({
+    username: {
+        type: DataTypes.STRING,
+        allowNull:false
+    },
+    email: {
+        type:DataTypes.STRING,
+        allowNull:false,
+        unique:true
+    },
+    password: {
+        type:DataTypes.STRING,
+        allowNull:false,
+        validate: {
+            len:[8]
+        }
+    }
+},{
+    sequelize
+});
+
+module.exports=User
