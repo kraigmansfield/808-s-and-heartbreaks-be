8aa5f780d653bb53adee5c43f03210a579433c0e
kraigmansfield <Kraig.mansfield@gmail.com>
2023-03-07 10:32:24 -0800

    began server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const allRoutes = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use('/',allRoutes);
+
+app.listen(PORT, function(){
+    console.log('App listening on PORT ' + PORT);
+});
